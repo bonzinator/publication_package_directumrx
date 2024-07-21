4b96cc1750e8413fe1bf1eccc86c0b35185edad1
Dmitriy Palchikov <dmitrij.palchikof@yandex.ru>
2024-07-21 12:37:24 +0300

    Обновление функций и основного файла. Улучшение логирования и читаемости.

--- function.py
+++ function.py
@@ -13,81 +13,53 @@ logging.basicConfig(filename='logfile.log', level=logging.INFO,
 
 def execute_command_on_server(hostname, port, username, password, command):
     """
-    Данная функция принимает в себя параметры для подключения к серверам по SSH и выполняет
-    определенную команду, которую укажет пользователь. Вся информация, пишется в лог файл logfile.log
+    Выполняет команду на удаленном сервере по SSH.
+    Логирует все этапы и ошибки.
     """
-
     try:
-        # Создание SSH клиента
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-        # Логирование этапа подключения
         logging.info(f"Connecting to {hostname} as {username}...")
-
-        # Подключение к серверу
         client.connect(hostname, port, username, password)
-
-        # Логирование успешного подключения
         logging.info(f"Connected to {hostname}")
-
-        # Выполнение команды
         logging.info(f"Executing command: {command}")
         stdin, stdout, stderr = client.exec_command(command)
-
-        # Чтение вывода команды
         output = stdout.read().decode()
         error = stderr.read().decode()
-
-        # Логирование вывода команды
         if output:
             logging.info(f"Command output:\n{output}")
         if error:
             logging.error(f"Command error:\n{error}")
-
-        # Закрытие соединения
         client.close()
         logging.info("Connection closed")
-
-        if output:
-            return True
-        else:
-            return False
-
+        return not bool(error)
     except paramiko.ssh_exception.AuthenticationException as e:
         logging.error(f"Authentication failed: {str(e)}")
-        return None, str(e)
     except paramiko.ssh_exception.SSHException as e:
         logging.error(f"SSH connection failed: {str(e)}")
-        return None, str(e)
     except Exception as e:
         logging.error(f"An error occurred: {str(e)}")
-        return None, str(e)
+    return False
 
 
 def command_os(script_path, parameter, path_dat=None):
     """
-    Запускает выполнение скрипта на сервере Linux с помощью os.system.
-    :param path_dat:
-    :param parameter:
-    :param script_path: Путь к скрипту, который нужно выполнить.
+    Запускает выполнение скрипта на сервере Linux.
+    Логирует все этапы и ошибки.
     """
     if parameter == 'haproxy':
-        logging.info(f"Command HAPROXY UP {script_path}")
         command = f"bash {script_path} haproxy up"
+        logging.info(f"Command HAPROXY UP {script_path}")
     elif parameter == 'deploy':
+        command = f"bash {script_path} dt deploy --init --package='{path_dat}'"
         logging.info(f"Command DEPLOY UP {script_path}")
-        f"bash {script_path} dt deploy --init --package='{path_dat}'"
     else:
         logging.error(f"Invalid parameter provided {parameter}")
         return False
 
-    # Выполнение команды по перезагрузке haproxy или deploy
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     output, error = process.communicate()
     exit_code = process.returncode
-
-    # Логирование вывода команды
     if output:
         logging.info(f"Command output:\n{output}")
     if error:
@@ -103,78 +75,68 @@ def command_os(script_path, parameter, path_dat=None):
 
 def check_file_exists(file_path):
     """
-    Данная функция принимает в себя путь к файлу разработки .dat.
-    Она запускает проверку на существование файла на определенном сервере.
+    Проверяет существование файла.
+    Логирует результат.
     """
-    if glob.glob(file_path):
+    exists = glob.glob(file_path)
+    if exists:
         logging.info(f"File {file_path} exists")
     else:
         logging.info(f"File {file_path} does not exist")
-    return glob.glob(file_path)
+    return bool(exists)
 
 
 def update_yaml(file_path, host_fqdn, protocol, ssl_cert, dns_name, extra_host):
     """
-    Функция изменяет параметры файла конфигурации для корректного проведения публикации.
-    Она подменяет параметры host_fqdn и protocol в секции variables для работы haproxy по http.
-    Также она удаляет и добавляет параметры в extra_hosts и ssl_cert соответственно.
+    Обновляет параметры в YAML файле.
+    Логирует все этапы и ошибки.
     """
     yaml = YAML()
     yaml.preserve_quotes = True
 
-    with open(file_path, 'r') as file:
-        data = yaml.load(file)
+    try:
+        with open(file_path, 'r') as file:
+            data = yaml.load(file)
 
-    # Обновление данных в параметрах host_fqdn и protocol
-    if 'variables' in data:
-        data['variables']['host_fqdn'] = host_fqdn
-        logging.info(f"Updating {file_path}, new host fqdn: {host_fqdn}")
-        data['variables']['protocol'] = protocol
-        logging.info(f"Updating {file_path}, new protocol: {protocol}")
+        if 'variables' in data:
+            data['variables']['host_fqdn'] = host_fqdn
+            data['variables']['protocol'] = protocol
+            logging.info(f"Updated variables: host_fqdn={host_fqdn}, protocol={protocol}")
 
-    # Добавление и удаление строчки ssl_cert в блоке HAProxy
-    if 'services_config' in data and 'SungeroHaproxy' in data['services_config']:
-        haproxy_config = data['services_config']['SungeroHaproxy']
-        if 'ssl_cert' in haproxy_config:
-            haproxy_config.pop('ssl_cert')
-            logging.info(f"Updating {file_path}, variable ssl_cert was deleted")
-        else:
+        if 'services_config' in data and 'SungeroHaproxy' in data['services_config']:
+            haproxy_config = data['services_config']['SungeroHaproxy']
             haproxy_config['ssl_cert'] = ssl_cert
-            logging.info(f"Updating {file_path}, ssl_cert = {ssl_cert}")
-        if 'https_port' not in haproxy_config:
             haproxy_config['https_port'] = '{{ https_port }}'
-            logging.info(f"Updating {file_path}, https_port = https_port ")
+            logging.info(f"Updated SungeroHaproxy: ssl_cert={ssl_cert}, https_port='{{ https_port }}'")
 
-    # Добавление и удаление параметра в блоке extra_hosts
-    if 'extra_hosts' in data and dns_name in data['extra_hosts']:
-        data['extra_hosts'].pop(dns_name)
-        logging.info(f"Updating {file_path}, extra_hosts was deleted")
-    else:
-        data['extra_hosts'][dns_name] = extra_host
-        logging.info(f"Updating {file_path}, extra_hosts was added to {dns_name}")
+        if 'extra_hosts' in data:
+            data['extra_hosts'][dns_name] = extra_host
+            logging.info(f"Updated extra_hosts: {dns_name}={extra_host}")
 
-    with open(file_path, 'w') as file:
-        yaml.dump(data, file)
+        with open(file_path, 'w') as file:
+            yaml.dump(data, file)
+        logging.info(f"YAML file {file_path} updated successfully")
+        return True
+    except Exception as e:
+        logging.error(f"Error updating YAML file {file_path}: {e}")
+        return False
 
 
 def delete_files_by_pattern(directory):
     """
-    Удаляет все файлы в указанной директории, соответствующие шаблону.
-
-    :param directory: Путь к директории, в которой нужно удалить файлы.
+    Удаляет файлы в указанной директории по шаблону.
+    Логирует все этапы и ошибки.
     """
     try:
-        # Поиск всех файлов, соответствующих шаблону
         files = glob.glob(directory)
-
         for file_path in files:
             try:
                 if os.path.isfile(file_path) or os.path.islink(file_path):
                     os.remove(file_path)
                     logging.info(f"File {file_path} was deleted")
                 else:
-                    logging.warning(f"{file_path} is a directory. We can't delete it")
+                    logging.warning(f"{file_path} is a directory and cannot be deleted")
             except Exception as e:
-                logging.error(f"File deletion error {file_path}: {e}")
+                logging.error(f"Error deleting file {file_path}: {e}")
     except Exception as e:
         logging.error(f"Error accessing directory {directory}: {e}")
--- main.py
+++ main.py
@@ -1,102 +1,102 @@
 # -*- coding: utf-8 -*-
 
+import sys
 import function as f
 import commands as c
 from configparser import ConfigParser
 
-# Читаем файл конфигурации, для того чтобы забрать оттуда реквизиты пользователя
-# и путь к скрипту
-config = ConfigParser()
-config.read('config.ini')
-# Получаем секцию с серверами
-servers = config['servers']
-# Получаем порт для SSH
-port = config.get("ssh", "port")
-# Получаем реквизиты пользователя через файл конфигурации
-username = config.get("props", "username")
-password = config.get("props", "password")
-# Получаем полный путь к пакету разработки и к скрипту do.sh
-path_for_do = config.get("paths", "path_for_do")
-file_path = config.get("paths", "path_for_package")
-# Получаем данные для подмены в файле config.yml
-config_yml_path = config.get("paths", "config_yml_path")
-new_host_fqdn = config.get("changes_in_yml", "new_host_fqdn")
-new_protocol = config.get("changes_in_yml", "new_protocol")
-old_host_fqdn = config.get("changes_in_yml", "old_host_fqdn")
-old_protocol = config.get("changes_in_yml", "old_protocol")
-ssl_cert = config.get("paths", "ssl_cert")
 
-if __name__ == "__main__":
+def load_config(file_path):
+    config = ConfigParser()
+    config.read(file_path)
+    return config
 
-    file_exists = f.check_file_exists(file_path=file_path)
 
-    if file_exists:
+def main():
+    config = load_config('config.ini')
 
-        if f.update_yaml(file_path=config_yml_path,
+    servers = config['servers']
+    port = config.get("ssh", "port")
+    username = config.get("props", "username")
+    password = config.get("props", "password")
+    path_for_do = config.get("paths", "path_for_do")
+    file_path = config.get("paths", "path_for_package")
+    config_yml_path = config.get("paths", "config_yml_path")
+    new_host_fqdn = config.get("changes_in_yml", "new_host_fqdn")
+    new_protocol = config.get("changes_in_yml", "new_protocol")
+    old_host_fqdn = config.get("changes_in_yml", "old_host_fqdn")
+    old_protocol = config.get("changes_in_yml", "old_protocol")
+    ssl_cert = config.get("paths", "ssl_cert")
+
+    if not f.check_file_exists(file_path=file_path):
+        print("File not exist")
+        sys.exit(1)
+
+    if not f.update_yaml(file_path=config_yml_path,
                          host_fqdn=new_host_fqdn,
                          protocol=new_protocol,
                          ssl_cert=ssl_cert,
                          dns_name=old_host_fqdn,
                          extra_host=new_host_fqdn):
-            print("Updated config file successfully")
+        print("Convert yaml finished with error")
+        sys.exit(1)
 
-            if f.command_os(script_path=path_for_do, parameter='haproxy'):
-                print("Running haproxy command successfully")
-                status_function_down = None
-                for server_name in servers:
-                    if f.execute_command_on_server(hostname=servers[server_name],
-                                                   port=port,
-                                                   username=username,
-                                                   password=password,
-                                                   command=c.all_down):
-                        print(f"{server_name} is down")
-                        status_function_down = True
-                    else:
-                        status_function_down = False
-                        for server_name_ in servers:
-                            f.execute_command_on_server(hostname=servers[server_name_],
-                                                        port=port,
-                                                        username=username,
-                                                        password=password,
-                                                        command=c.all_up)
-                        print(f"{server_name} down exited with error")
-                        break
+    print("Updated config file successfully")
 
-                if status_function_down:
-                    if f.command_os(script_path=path_for_do, parameter='deploy', path_dat=file_path):
-                        print("Running deployment command successfully")
+    if not f.command_os(script_path=path_for_do, parameter='haproxy'):
+        print("Haproxy Up Failed")
+        sys.exit(1)
 
-                        status_function_up = None
-                        for server_name in servers:
-                            if f.execute_command_on_server(hostname=servers[server_name],
-                                                           port=port,
-                                                           username=username,
-                                                           password=password,
-                                                           command=c.all_up):
-                                print(f"{server_name} is up")
-                                status_function_up = True
-                            else:
-                                status_function_up = False
-                                for server_name_ in servers:
-                                    f.execute_command_on_server(hostname=servers[server_name_],
-                                                                port=port,
-                                                                username=username,
-                                                                password=password,
-                                                                command=c.all_up)
-                                print(f"{server_name} up exited with error")
-                                break
-                        if status_function_up:
-                            print("Status function up is True")
-                            f.delete_files_by_pattern(directory=file_path)
-                        else:
-                            print("Status function up is False")
-                    else:
-                        print("deploy exited with error")
-                else:
-                    print("Status function down is False")
-            else:
-                print("Haproxy Up Failed")
+    print("Running haproxy command successfully")
+
+    if not execute_commands_on_servers(servers, port, username, password, c.all_down, "down"):
+        sys.exit(1)
+
+    if not f.command_os(script_path=path_for_do, parameter='deploy', path_dat=file_path):
+        print("Deploy exited with error")
+        sys.exit(1)
+
+    print("Running deployment command successfully")
+
+    if not f.update_yaml(file_path=config_yml_path,
+                         host_fqdn=old_host_fqdn,
+                         protocol=old_protocol,
+                         ssl_cert=ssl_cert,
+                         dns_name=old_host_fqdn,
+                         extra_host=new_host_fqdn):
+        print("Convert yaml finished with error")
+        sys.exit(1)
+
+    if not f.command_os(script_path=path_for_do, parameter='haproxy'):
+        print("Haproxy Up Failed")
+        sys.exit(1)
+
+    if not execute_commands_on_servers(servers, port, username, password, c.all_up, "up"):
+        sys.exit(1)
+
+    print("Status function up is True")
+    f.delete_files_by_pattern(directory=file_path)
+
+
+def execute_commands_on_servers(servers, port, username, password, command, action):
+    for server_name in servers:
+        if f.execute_command_on_server(hostname=servers[server_name],
+                                       port=port,
+                                       username=username,
+                                       password=password,
+                                       command=command):
+            print(f"{server_name} is {action}")
         else:
-            print("Convert yaml finished with error")
-    else:
-        print("File not exist")
+            for server_name_ in servers:
+                f.execute_command_on_server(hostname=servers[server_name_],
+                                            port=port,
+                                            username=username,
+                                            password=password,
+                                            command=c.all_up)
+            print(f"{server_name} {action} exited with error")
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    main()
